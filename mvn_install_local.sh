3a8c4e276bd596fffb9d3e5d482a1bbdc0389917
Jenkins Autobot <releaseengineering@magnet.com>
2015-06-19 20:40:23 +0000

    Version release-1.0.2-RC10

--- mvn_install_local.sh
+++ mvn_install_local.sh
@@ -2,8 +2,8 @@
 
 # Deploy openfire.jar to local maven repository
 CURDIR=`pwd`
-ARTIFACT_VERSION="1.0.1"
-FILE="${CURDIR}/target/openfire/lib/openfire.jar" 
+ARTIFACT_VERSION="1.0.2-SNAPSHOT"
+FILE="${CURDIR}/target/openfire/lib/openfire.jar"
 
 cd ${CURDIR}/target
 zip -r mmx-openfire.zip openfire
